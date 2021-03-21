ca83e8e0057f8db811afff720d706576d0a5478e
Jerry Yu <jerryyu8@gmail.com>
2021-03-20 18:33:24 -0700

    Add prettier ignore

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,9 +1,5 @@
 import React, { useState, Fragment } from "react";
 import { withStyles } from "@material-ui/core/styles";
-
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-
 import styles from "./styles";
 
 function Home(props) {
@@ -12,11 +8,9 @@ function Home(props) {
 
   return (
     <div className={!isMobile ? classes.component : classes.componentMobile}>
-            <h1 className={classes.h1}>Jerry Yu</h1>
-            <p className={classes.p}>
-              Software Engineer at Instagram
-            </p>
-            <a href={process.env.PUBLIC_URL + "/resume.pdf"}>Resume</a>
+      <h1 className={classes.h1}>Jerry Yu</h1>
+      <p className={classes.p}>Software Engineer at Instagram</p>
+      <a href={process.env.PUBLIC_URL + "/resume.pdf"}>Resume</a>
     </div>
   );
 }
