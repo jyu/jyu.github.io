18ce03999729e25668ab63eaca9363f06d33bc66
Jerry Yu <jerryyu8@gmail.com>
2021-03-21 00:37:28 -0700

    Fix some project links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./routes/Home";
 import Food from "./routes/Food";
 import Projects from "./routes/Projects";
+import SpotRecs from "./routes/ProjectPages/SpotRecs";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/food" component={Food} />
         <Route exact path="/projects" component={Projects} />
+        <Route exact path="/SpotRecs" component={SpotRecs} />
       </Switch>
     </Router>
   );
---
+++ src/routes/ProjectPages/SpotRecs/index.jsx
@@ -0,0 +1,4 @@
+export default function Spotrecs(props) {
+  window.location.href = "https://github.com/jyu/spotrecs";
+  return null;
+}
