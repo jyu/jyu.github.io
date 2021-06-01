149b16865a77651c632e761147a1aa7be3183563
Jerry Yu <jerryyu8@gmail.com>
2021-06-01 05:35:05 -0700

    Add abc kitchen

--- src/data/abckitchen.js
+++ src/data/abckitchen.js
@@ -10,14 +10,20 @@ const place = {
     style: "American",
     lastVisited: "2021-05-28",
     text: [
-      "ABC kitchen is an American restaurant that strives to source the freshest ingredients from local farms and their rooftop farm. "
+      "ABC kitchen is an American restaurant that strives to source the freshest ingredients from local farms and their rooftop farm. The restaurant has an interesting story as originally part of the ABC Carpet & Home furniture store and now part of the ABC juggernaut along with ABC Cocina and AbcV. The restaurant has a rustic atmosphere inside, combining farmhouse and club vibes in a dimly lit space. As claimed on their website, their dining room features 'salvaged and reclaimed equipment', and our abstract log-shaped table reflected that.",
+      "The first time I went, I don't remember much except that the crab toast was excellent and the ravioli I had was light and delicious. I'll write about my most recent trip to ABC kitchen.",
+      "We started with the Peekytoe crab toast with lemon aioli, which was delicious. There was lots of fresh crab, which provided a fresh slightly sweetness and deep crab flavor. The crab was mixed with fresh herbs and the aioli provided just the right amount of creaminess to bring the crab together. The toast provided a great crunch and added substance to the crab. Fresh lemon on top provided just the right acidity to cut through everything. This was probably one of the best pieces of toast I've had.",
+      "For the main, I ordered fried organic chicken, rainbow swiss chard habanero hot sauce butter. The dish looked stunning with a golden crust, but I was a bit disappointed with the dish. The chicken was made out of chicken breast, which made me feel healthy eating fried chicken but was not the main purpose of this meal. The chicken breast was juicy, but its low fat content and awkward crust to meat ratio made me want chicken thigh. In terms of flavor, the habanero hot sauce butter was good, but I wanted more bang (spice) and more sauce. The swiss chard tasted great, but it was another vehicle for the sauce in the dish. Maybe fried chicken wasn't the right thing to order here, but the price and the abundance of so much great fried chicken cause this dish to lose some points.",
+      "For dessert, we shared the cookie plate among 3 people, which had: cherry blossom sable’, caramel swirl brownie, snickerdoodle, brown butter chocolate chip, oatmeal sandwich cookie. My friends are big cookie fans, and the cookie plate was a great selection to try out different flavors and satisfy their craving. The standouts were the soft brownie, the snickerdoodle, and the oatmeal sandwich with its marshmellow filling. A minor nitpick is that the cookies would be better if they were toasted quickly, but was a great way to end our meal.",
+      "Overall, I think ABC Kitchen has a great take on fresh farm-to-table American food. For me, it falls into a few of the common pitfalls of American food of not having enough flavor or being expensive. But I would be willing to give it another shot and maybe order like 5 crab toasts (jkjk). Some of their salads and pizzas look great and I think it would be a better way to experience the freshness of what ABC kitchen has to offer."
     ],
     dishes: [
-      "Pepperoni Slice ($5.50) - Corner slice, Salty peps, fluffy sourdough, lots of char",
+      "Crab Toast ($19) - Peekytoe crab toast with lemon aioli ",
+      "Fried Chicken ($33) - fried organic chicken, rainbow swiss chard, habanero hot sauce butter",
+      "Cookie plate ($14) - cherry blossom sable’, caramel swirl brownie, snickerdoodle, brown butter chocolate chip, oatmeal sandwich cookie"
     ],
     images: 3,
-    captions: ["Pepperoni Slice"],
-    video: "https://www.youtube.com/watch?v=cTW1T3GAH_s?t=20",
+    captions: ["Peekytoe crab toast with lemon aioli", "Fried organic chicken, rainbow swiss chard, habanero hot sauce butter", "Cookie plate"],
     times: 2,
   },
 };
